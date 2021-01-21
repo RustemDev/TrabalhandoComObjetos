54ee50bd9a07df19833e32045a80f00fb7ce2290
RustemDev <rhquestion89@gmail.com>
2021-01-21 10:24:45 -0300

    Foi criado varios objetos

---
+++ TrabalhandoComObjetos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabalhandoComObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
